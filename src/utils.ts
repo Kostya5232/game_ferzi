f77797e0bd4a1b05242f4cd62335d75a2290a7b7
Константин <83291160+Kostya5232@users.noreply.github.com>
2024-12-16 13:15:59 +0400

    build

--- src/utils.ts
+++
@@ -1,5 +0,0 @@
-export type Position = { row: number; col: number };
-
-export const isUnderAttack = (queens: Position[], row: number, col: number): boolean => {
-    return queens.some((q) => q.row === row || q.col === col || Math.abs(q.row - row) === Math.abs(q.col - col));
-};
